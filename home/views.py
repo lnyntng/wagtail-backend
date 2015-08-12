b2ee3233475475837ce29772ea877ba5006cda78
lnyntng <lnyntng@gmail.com>
2015-08-12 11:10:07 -0600

    Comment get method

--- home/views.py
+++ home/views.py
@@ -7,18 +7,19 @@ from wagtail.wagtailcore.models import Page
 from wagtail.wagtailsearch.models import Query, EditorsPick
 
 def get(request, category):
-    filtered_pages = []
+	pass
+    # filtered_pages = []
 
-    if category is 'signature':
-    	filtered_pages = HomePage.objects.live().filter(category='signature')
-    elif category is 'discover':
-    	filtered_pages = HomePage.objects.live().filter(category='discover')
-    else:
-    	filtered_pages = HomePage.objects.live()
+    # if category is 'signature':
+    # 	filtered_pages = HomePage.objects.live().filter(category='signature')
+    # elif category is 'discover':
+    # 	filtered_pages = HomePage.objects.live().filter(category='discover')
+    # else:
+    # 	filtered_pages = HomePage.objects.live()
 
-    response_data = []
-    for page in filtered_pages:
-    	for rawhtml in page.body:
-    		response_data.append({'content': body, 'tags': category})
+    # response_data = []
+    # for page in filtered_pages:
+    # 	for rawhtml in page.body:
+    # 		response_data.append({'content': body, 'tags': category})
 
-    return HttpResponse(json.dumps(response_data), content_type="application/json")
+    # return HttpResponse(json.dumps(response_data), content_type="application/json")
