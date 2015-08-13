499e11e4ada54552cf82dbbc1a0087329c8a56fd
lnyntng <lnyntng@gmail.com>
2015-08-13 15:44:59 -0600

    Update password

--- wagtail_backend/settings/production.py
+++ wagtail_backend/settings/production.py
@@ -16,7 +16,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'd8o4ts9rgsv55v',
         'USER': 'ggupiyudrbkhur',
-        'PASSWORD': 'ggupiyudrbkhur',
+        'PASSWORD': 'EcoJwSBQmkwOtLff31radhtWIg',
         'HOST': 'ec2-54-83-51-0.compute-1.amazonaws.com',
         'PORT': '5432',
     }
