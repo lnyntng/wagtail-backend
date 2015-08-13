1d064693e96f9563a8af6dcd85cd4da02882cb17
lnyntng <lnyntng@gmail.com>
2015-08-13 15:28:27 -0600

    Add environment for wagtail, change database

---
+++ ubuntu.sh
@@ -0,0 +1,285 @@
+
+
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="x-ua-compatible" content="ie=edge" />
+        <title> | Wagtail CMS</title>
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@wagtailcms" />
+        <meta name="twitter:creator" content="@wagtailcms" />
+        <meta name="twitter:title" content=" | Wagtail CMS" />
+        <meta name="twitter:description" content="" />
+        <meta name="twitter:image:src" content="https://wagtail.io//static/img/default-sharing-image.jpg" />
+
+        <meta property="fb:app_id" content="710333029076622" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://wagtail.io/" />
+        <meta property="og:title" content=" | Wagtail CMS" />
+        <meta property="og:image" content="https://wagtail.io//static/img/default-sharing-image.jpg" />
+        <meta property="og:description" content="" />
+        <meta property="og:site_name" content="Wagtail CMS" />
+
+        <link rel="apple-touch-icon" sizes="57x57" href="/static/img/favicons/apple-touch-icon-57x57.png"/>
+        <link rel="apple-touch-icon" sizes="60x60" href="/static/img/favicons/apple-touch-icon-60x60.png"/>
+        <link rel="apple-touch-icon" sizes="72x72" href="/static/img/favicons/apple-touch-icon-72x72.png"/>
+        <link rel="apple-touch-icon" sizes="76x76" href="/static/img/favicons/apple-touch-icon-76x76.png"/>
+        <link rel="apple-touch-icon" sizes="114x114" href="/static/img/favicons/apple-touch-icon-114x114.png"/>
+        <link rel="apple-touch-icon" sizes="120x120" href="/static/img/favicons/apple-touch-icon-120x120.png"/>
+        <link rel="apple-touch-icon" sizes="144x144" href="/static/img/favicons/apple-touch-icon-144x144.png"/>
+        <link rel="apple-touch-icon" sizes="152x152" href="/static/img/favicons/apple-touch-icon-152x152.png"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/static/img/favicons/apple-touch-icon-180x180.png"/>
+        <link rel="icon" type="image/png" href="/static/img/favicons/favicon-32x32.png" sizes="32x32"/>
+        <link rel="icon" type="image/png" href="/static/img/favicons/android-chrome-192x192.png" sizes="192x192"/>
+        <link rel="icon" type="image/png" href="/static/img/favicons/favicon-96x96.png" sizes="96x96"/>
+        <link rel="icon" type="image/png" href="/static/img/favicons/favicon-16x16.png" sizes="16x16"/>
+        <link rel="manifest" href="/static/img/favicons/manifest.json" />
+        <link rel="shortcut icon" href="/static/img/favicons/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#43b1b0"/>
+        <meta name="msapplication-TileImage" content="/static/img/favicons/mstile-144x144.png"/>
+        <meta name="msapplication-config" content="/static/img/favicons/browserconfig.xml" />
+        <meta name="theme-color" content="#43b1b0"/>
+
+        <!-- Font: Effra -->
+        <script src="//use.typekit.net/mah8zuf.js"></script>
+        <script>try{Typekit.load();}catch(e){}</script>
+        
+        <link rel="stylesheet" href="/static/CACHE/css/f4738925d964.css" type="text/css" />
+        
+
+    </head>
+    <body class=" template-404">
+        
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5XDDLH"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5XDDLH');</script>
+        
+        
+        <div id="fb-root"></div>
+        <script>
+            window.fbAsyncInit = function() {
+                FB.init({
+                    appId: 710333029076622,
+                    xfbml: true,
+                    version: 'v2.1'
+                });
+            };
+
+            (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        </script>
+
+        
+
+        <!--[if lt IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        
+
+    <div class="page">
+
+        <header>
+            <div class="container">
+                <a id="branding" href="/"><img src="/static/img/wagtail.svg" alt="Wagtail" /></a>
+                <a href="#primary_navigation">Menu</a>
+                
+
+<nav class="primary_navigation">
+	<ul>
+    
+    	<li>
+        	<a href="/features/">features</a>
+        </li>
+    
+    	<li>
+        	<a href="/about-wagtail/">about wagtail</a>
+        </li>
+    
+    	<li>
+        	<a href="/services/">services</a>
+        </li>
+    
+    	<li>
+        	<a href="/blog/">blog</a>
+        </li>
+    
+    	<li>
+        	<a href="/developers/">developers</a>
+        </li>
+    
+    	<li>
+    		<a href="/developers/" class="get_started"><span class="fa fa-download icon"></span>Get started</a>
+    	</li>
+    </ul>
+</nav>
+            </div>
+        </header>
+        
+        
+
+        <div class="body">
+
+            <section>
+                
+    <h1>Page not found</h1>
+    <p>Sorry, this page could not be found.</p>
+
+            </section>
+            
+            <footer>
+                <div class="container">
+                    <nav class="get_started_links">
+    <h3>Good to go?</h3>
+    <p>Everything you need to know about getting your Wagtail build up and running</p>
+
+    
+        <a href="http://docs.wagtail.io/"><span class="fa fa-eye"></span> Documentation</a>
+      
+</nav>
+                    <nav class="network_links">
+    <h3>Network </h3>
+    <ul>
+        
+            <li>
+                <a href="https://torchbox.com/">Torchbox</a>
+                <p>Talk to Torchbox, the creators of Wagtail.</p>
+            </li>
+        
+            <li>
+                <a href="http://madewithwagtail.org/">Made with Wagtail</a>
+                <p>A showcase of sites and apps made with Wagtail CMS.</p>
+            </li>
+        
+    </ul>
+</nav>
+                    
+
+<div class="latest_blog">
+	<h3>Blog</h3>
+	
+        <ul>
+            <li>
+                <span class="date">July 29, 2015</span>
+                <a class="title" href="/blog/deploying-wagtail-heroku/">Deploying Wagtail to Heroku</a>
+                
+                    <div class="author">
+                        <span class="image">
+                            <img src="/media/images/Profile_pic.2e16d0ba.fill-55x55.jpg" width="55" height="55" alt="Chris Rogers" alt="Chris Rogers">
+                        </span>
+                        <span class="details">
+                            <a class="name" href="https://torchbox.com/team/chris-rogers">Chris Rogers</a>
+                            <span class="role">Project manager</span>
+                        </span>
+                    </div>
+                
+            </li>
+        </ul>
+	
+</div>
+
+                    <form class="newsletter_signup" action="/newsletter-signup" method="post">
+    <input type='hidden' name='csrfmiddlewaretoken' value='xH3X5tTVYYy7Jy6rCmvUpZxjRqAYHJO5' />
+    <h3>Stay up to date with Wagtail</h3>
+    <fieldset>
+        <input name="email" type="text" placeholder="Enter your email" />
+        <button>Sign up</button>
+    </fieldset>
+</form>
+
+                    <nav class="follow_links">
+	<h3>Follow</h3>
+	<a href="https://github.com/torchbox/wagtail/" class="fa fa-github icon"></a>
+	<a href="https://twitter.com/wagtailcms" class="fa fa-twitter icon"></a>
+</nav>
+
+                    
+
+<div class="footnote">
+	<p class="copyright">Wagtail is an open source project by Torchbox. © 2015</p>
+	<a class="logo" href="http://torchbox.com"><img src="/static/img/torchbox.svg" alt="Torchbox" /></a>
+</div>
+                </div>
+            </footer>
+            
+        </div>
+
+        <div id="menu_overlay">
+            
+
+<nav class="primary_navigation">
+	<ul>
+    
+    	<li>
+        	<a href="/features/">features</a>
+        </li>
+    
+    	<li>
+        	<a href="/about-wagtail/">about wagtail</a>
+        </li>
+    
+    	<li>
+        	<a href="/services/">services</a>
+        </li>
+    
+    	<li>
+        	<a href="/blog/">blog</a>
+        </li>
+    
+    	<li>
+        	<a href="/developers/">developers</a>
+        </li>
+    
+    	<li>
+    		<a href="/developers/" class="get_started"><span class="fa fa-download icon"></span>Get started</a>
+    	</li>
+    </ul>
+</nav>
+        </div>
+
+    </div>
+
+
+
+        Copyright &copy; 2015 Torchbox Ltd
+
+
+        <!-- twitter campaign tracking code -->
+        <script src="https://platform.twitter.com/oct.js"></script>
+        <script>
+            twttr.conversion.trackPid('l6ggy', {
+                tw_sale_amount: 0, 
+                tw_order_quantity: 0 
+            });
+        </script>
+        <noscript>
+            <img height="1" width="1" style="display:none;" alt=""
+            src="https://analytics.twitter.com/i/adsct?txn_id=l6ggy&p_id=Twitter&tw_sale_amount=0&tw_order_quantity=0"
+            />
+            <img height="1" width="1" style="display:none;" alt=""
+            src="//t.co/i/adsct?txn_id=l6ggy&p_id=Twitter&tw_sale_amount=0&tw_order_quantity=0"
+            />
+        </noscript>
+        <!-- end twitter campaign tracking code -->
+
+        <script src="https://platform.twitter.com/widgets.js" async></script>
+
+        <script type="text/javascript" src="/static/CACHE/js/14549ce2ab94.js"></script>        
+
+        
+    </body>
+</html>
+
--- wagtail_backend/settings/base.py
+++ wagtail_backend/settings/base.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'wagtail_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wagtail',
+        'USER': 'postgres',
+        'PASSWORD': '123qwe',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
