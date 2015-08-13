80b8262b209860d08e61f802d21887171f6358a5
lnyntng <lnyntng@gmail.com>
2015-08-13 15:41:19 -0600

    Add production database settings

--- wagtail_backend/settings/production.py
+++ wagtail_backend/settings/production.py
@@ -9,3 +9,15 @@ try:
     from .local import *
 except ImportError:
     pass
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd8o4ts9rgsv55v',
+        'USER': 'ggupiyudrbkhur',
+        'PASSWORD': 'ggupiyudrbkhur',
+        'HOST': 'ec2-54-83-51-0.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
